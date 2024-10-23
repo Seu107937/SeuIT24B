29597fe59582718332b815237d5fca71cd022fe0
Seu107937 <seu107937@donga.edu.vn>
2024-10-23 21:48:01 +0700

    Add files via upload

---
+++ thongTin.java
@@ -0,0 +1,22 @@
+package buoi3;
+public class thongTin {
+    public static void main(String[] args) {
+        int maSV = 107937;
+        String hoVaTen = "Seu";
+        String ngaySinh= "15/01/2006";
+        int tuoi = 18;
+        boolean nam = true;
+        String lop="IT24B";
+        String khoa="Cong Nghe Thong Tin";
+        String diachi="33 Xo Viet Nghe Tinh";
+        System.out.println("---THONG TIN SINH VIEN---");
+        System.out.println("ma sinh vien: " + maSV);
+        System.out.println("ho va ten: " + hoVaTen);
+        System.out.println("ngay sinh: " + ngaySinh);
+        System.out.println("tuoi: " + tuoi);
+        System.out.println("nam: " + nam);
+        System.out.println("lop: " + lop);
+        System.out.println("khoa: " + khoa);
+        System.out.println("---HET---");} 
+          
+}
